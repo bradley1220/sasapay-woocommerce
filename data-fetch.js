907fc4005e6900a7a1abd1f7ed8d4c0f85c18dbc
bradley1220 <129550942+bradley1220@users.noreply.github.com>
2025-05-27 10:54:29 +0300

    Update data-fetch.js

--- data-fetch.js
+++ data-fetch.js
@@ -1,1 +1,10 @@
-
+async function getWordPressData() {
+  try {
+    const response = await fetch('https://https://sasapay.co.ke/wp-json/wp/v2/posts');
+    const posts = await response.json();
+    return posts;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return [];
+  }
+}
